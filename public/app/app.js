40931d10070b1667218c7ac5e3611d8737eb7585
Jan Milosh <janmilosh@gmail.com>
2014-03-16 16:38:54 -0400

    Adds ui

--- public/app/app.js
+++ public/app/app.js
@@ -5,10 +5,6 @@ angular.module('app').config(function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl: '/partials/main',
-			controller: 'mainCtrl'
+			controller: 'mvMainCtrl'
 		});
-});
-
-angular.module('app').controller('mainCtrl', function($scope) {
-	$scope.myVar = "Hello Angular";
 });
